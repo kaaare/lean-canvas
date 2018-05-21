b607147e76dc3d87f2cac1e2edf8d26d25ba007a
kaaare <kaaare@gmail.com>
2018-05-21 23:02:21 +0200

    Modified start_url and cache values to allow site to work without a trailing dash in the url.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('leanCanvas').then(function(cache) {
      return cache.addAll([
-       "/",
+       ".",
        "https://cdnjs.cloudflare.com/ajax/libs/pouchdb/6.4.3/pouchdb.min.js"
      ]);
    })
