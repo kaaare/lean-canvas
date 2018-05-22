16d93de75036cdefeb483692cda1326a7a155810
kaaare <kaaare@gmail.com>
2018-05-22 22:34:42 +0200

    Changed all unicode icons to Material icons from the official web font. Updated sw.js to cache the web font.

--- sw.js
+++ sw.js
@@ -3,7 +3,8 @@ self.addEventListener('install', function(e) {
    caches.open('leanCanvas').then(function(cache) {
      return cache.addAll([
        ".",
-       "https://cdnjs.cloudflare.com/ajax/libs/pouchdb/6.4.3/pouchdb.min.js"
+       "https://cdnjs.cloudflare.com/ajax/libs/pouchdb/6.4.3/pouchdb.min.js",
+       "https://fonts.googleapis.com/icon?family=Material+Icons"
      ]);
    })
  );
