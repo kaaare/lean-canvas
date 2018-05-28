9e65617e851f7dfea587f4f5223d2481021957a7
kaaare <kaaare@gmail.com>
2018-05-28 13:18:17 +0200

    Moved icons to separate folder and updated app manifest. Added a favicon.ico made from icon48.png to root. Added comments to sw.js, fixed a naming inconsistency ("e" vs "event"), and made arrow functions of the function expressions.

--- sw.js
+++ sw.js
@@ -1,19 +1,23 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('leanCanvas').then(function(cache) {
-     return cache.addAll([
-       ".",
-       "https://cdnjs.cloudflare.com/ajax/libs/pouchdb/6.4.3/pouchdb.min.js",
-       "https://fonts.googleapis.com/icon?family=Material+Icons"
-     ]);
-   })
- );
+self.addEventListener('install', event => {
+  event.waitUntil(
+    // Opening a named cache autovivifies the actual cache.
+    caches.open('leanCanvas').then(cache => {
+      return cache.addAll([
+        ".",
+        "https://cdnjs.cloudflare.com/ajax/libs/pouchdb/6.4.3/pouchdb.min.js",
+        "https://fonts.googleapis.com/icon?family=Material+Icons"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
+// Handle (intercept) requests.
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(response => {
+      // Prioritise cached file || if not found then fetch new file.
       return response || fetch(event.request);
+      // This would be in the opposite order for an app with frequently updated resources.
     })
   );
 });
